fc6d7fcc88fc9bfc75501bc6a630d618e121463e
Mike Hagedorn <mike@silverchairsolutions.com>
2012-11-05 07:47:54 -0600

    first commit

---
+++ SoundTouchTests/SoundTouchTests.h
@@ -0,0 +1,13 @@
+//
+//  SoundTouchTests.h
+//  SoundTouchTests
+//
+//  Created by Michael Hagedorn on 11/3/12.
+//  Copyright (c) 2012 Michael Hagedorn. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SoundTouchTests : SenTestCase
+
+@end
---
+++ SoundTouchTests/SoundTouchTests.m
@@ -0,0 +1,32 @@
+//
+//  SoundTouchTests.m
+//  SoundTouchTests
+//
+//  Created by Michael Hagedorn on 11/3/12.
+//  Copyright (c) 2012 Michael Hagedorn. All rights reserved.
+//
+
+#import "SoundTouchTests.h"
+
+@implementation SoundTouchTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SoundTouchTests");
+}
+
+@end
